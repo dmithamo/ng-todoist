f7c031b93442aefb8b2b1d7789add9a24e4f6205
dmithamo <dennisbmithamo@gmail.com>
2020-08-08 14:00:55 +0300

    feat(toggle-style): add toggle for layout style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faCreditCard } from '@fortawesome/free-regular-svg-icons';
+import { faThList } from '@fortawesome/free-solid-svg-icons';
 import { AppComponent } from './app.component';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-
 @NgModule({
   declarations: [AppComponent, TodoListComponent, CopyrightComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faThList, faCreditCard);
+  }
+}
